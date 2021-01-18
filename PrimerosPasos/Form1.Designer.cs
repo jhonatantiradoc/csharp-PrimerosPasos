965afeea1757710a182186115cd2e409835d7174
jhonatantiradoc <jhonatantirado4@gmail.com>
2021-01-18 09:52:12 -0500

    Primer proyecto - calculadora básica

---
+++ PrimerosPasos/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace PrimerosPasos
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtSuma = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtResta = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMulti = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtDiv = new System.Windows.Forms.TextBox();
+            this.btnClean = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Número 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Número 2:";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(159, 42);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 23);
+            this.txtNum1.TabIndex = 2;
+            this.txtNum1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNum1_KeyPress);
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(159, 95);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 23);
+            this.txtNum2.TabIndex = 3;
+            this.txtNum2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNum2_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.button1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button1.Location = new System.Drawing.Point(37, 160);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 31);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "CALCULAR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtSuma
+            // 
+            this.txtSuma.Enabled = false;
+            this.txtSuma.Location = new System.Drawing.Point(159, 230);
+            this.txtSuma.Name = "txtSuma";
+            this.txtSuma.Size = new System.Drawing.Size(100, 23);
+            this.txtSuma.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 238);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Suma:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 294);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Resta:";
+            // 
+            // txtResta
+            // 
+            this.txtResta.Enabled = false;
+            this.txtResta.Location = new System.Drawing.Point(159, 286);
+            this.txtResta.Name = "txtResta";
+            this.txtResta.Size = new System.Drawing.Size(100, 23);
+            this.txtResta.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 342);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Multiplicación:";
+            // 
+            // txtMulti
+            // 
+            this.txtMulti.Enabled = false;
+            this.txtMulti.Location = new System.Drawing.Point(159, 334);
+            this.txtMulti.Name = "txtMulti";
+            this.txtMulti.Size = new System.Drawing.Size(100, 23);
+            this.txtMulti.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(37, 389);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 15);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "División:";
+            // 
+            // txtDiv
+            // 
+            this.txtDiv.Enabled = false;
+            this.txtDiv.Location = new System.Drawing.Point(159, 381);
+            this.txtDiv.Name = "txtDiv";
+            this.txtDiv.Size = new System.Drawing.Size(100, 23);
+            this.txtDiv.TabIndex = 12;
+            // 
+            // btnClean
+            // 
+            this.btnClean.Location = new System.Drawing.Point(175, 160);
+            this.btnClean.Name = "btnClean";
+            this.btnClean.Size = new System.Drawing.Size(118, 31);
+            this.btnClean.TabIndex = 13;
+            this.btnClean.Text = "LIMPIAR";
+            this.btnClean.UseVisualStyleBackColor = true;
+            this.btnClean.Click += new System.EventHandler(this.btnClean_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 475);
+            this.Controls.Add(this.btnClean);
+            this.Controls.Add(this.txtDiv);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtMulti);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtResta);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSuma);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtSuma;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtResta;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMulti;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtDiv;
+        private System.Windows.Forms.Button btnClean;
+    }
+}
+
---
+++ PrimerosPasos/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimerosPasos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(txtNum1.Text != "" && txtNum2.Text != "")
+            {
+                double num1, num2;
+                double suma, resta, multiplicacion, division;
+                num1 = double.Parse(txtNum1.Text);
+                num2 = Convert.ToDouble(txtNum2.Text);
+                suma = num1 + num2;
+                resta = num1 - num2;
+                multiplicacion = num1 * num2;
+                division = num1 / num2;
+                txtSuma.Text = "" + suma;
+                txtResta.Text = "" + resta;
+                txtMulti.Text = Convert.ToString(multiplicacion);
+                txtDiv.Text = division.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Llene los campos", "Aviso del Sistema", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtNum1.Focus();
+            }      
+        }
+
+        private void btnClean_Click(object sender, EventArgs e)
+        {
+            txtNum1.Text = "";
+            txtNum2.Text = "";
+            txtSuma.Text = "";
+            txtResta.Text = "";
+            txtMulti.Text = "";
+            txtDiv.Text = "";
+            txtNum1.Focus();
+        }
+
+        private void txtNum1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                            (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtNum2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                            (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
